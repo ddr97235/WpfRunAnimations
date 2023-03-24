f8c4af57f0ab4d1d6b355a1b8da03122d3c05f88
ddr97235@hotmail.com <ddr97235@hotmail.com>
2023-03-24 14:37:51 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows;
+
+
+namespace WpfRunAnimations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using WpfRunAnimations.ViewModel.Simplified;
+using System.Windows;
+
+namespace WpfRunAnimations.ViewModel
+{
+    internal class MainWindowViewModel : BaseInpc
+    {
+
+        private RelayCommand? buttonClick;
+        public RelayCommand ButtonClick => buttonClick
+           ??= new RelayCommand(() =>
+           {
+               DoStartAnimation();
+           });
+
+        private Visibility bordeVisibility = Visibility.Collapsed;
+        public Visibility BordeVisibility
+        {
+            get => bordeVisibility;
+            set => Set(ref bordeVisibility, value);
+        }
+
+        private bool isNeedStartAnimation = false;
+        public bool IsNeedStartAnimation
+        {
+            get=> isNeedStartAnimation;
+            set=> Set(ref isNeedStartAnimation, value);
+        }
+
+        private void DoStartAnimation()
+        {
+            IsNeedStartAnimation = !IsNeedStartAnimation;
+            BordeVisibility = Visibility.Visible;
+        }
+    }
+}
